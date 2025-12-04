9a1d88a23264082972aa50be889d349f93e62777
jongnoh <whdshgm@gmail.com>
2025-12-04 10:43:01 +0900

    processClaim parameter fixed

--- controllers/musinsa.controller.js
+++ controllers/musinsa.controller.js
@@ -131,9 +131,9 @@ class MusinsaController  {
     }
     processClaim = async (req,res) => {
         try {
-            const claimNumber = req.body.claimNumber;
+            const orderNumber = req.body.orderNumber;
             const orderOptNumber = req.body.orderOptNumber;
-            const process = await this.musinsaService.processClaim(claimNumber, orderOptNumber);
+            const process = await this.musinsaService.processClaim(orderNumber, orderOptNumber);
             return res.status(200).json(process);
         } catch (error) {
             console.error('Process Claim 오류:', error);
